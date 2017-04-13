705e84ba6e16026a99d8ce94ecd23d5af072636f
My first commit <glendamacqueen@dyn129-100-89-86.ohr.reznet.uwo.ca>
2017-04-13 01:57:55 -0400

    my first commit

--- Swift first app/ViewController.swift
+++ Swift first app/ViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var FirstLabel: UILabel!
 
+    var tapcount = 0
+    
+    @IBAction func Pushed(_ sender: Any) {
+        tapcount = tapcount + 1
+       
+        if tapcount == 1 {
+            FirstLabel.text = "4"
+        }
+        if tapcount == 2 {
+            FirstLabel.text = "3"
+        }
+        if tapcount == 3 {
+            FirstLabel.text = "2"
+        }
+        if tapcount == 4 {
+            FirstLabel.text = "1"
+        }
+        if tapcount == 5 {
+            FirstLabel.text = "Fuck Paden"
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
-
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -22,4 +52,5 @@ class ViewController: UIViewController {
 
 
 }
+
 
